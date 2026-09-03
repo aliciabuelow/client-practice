91a318a5a4e189a92191f93fe0d6e1c208068b4d
Alicia Buelow <alicia@buelow.com.au>
2026-09-03 17:27:21 +0800

    Style filters, adjust header spacing

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,15 @@
+import '../styles/Filters.css';
+
 export default function Filters() {
     return (
         <div className="Filters">
-            <button>All</button>
-            <button>Not Requested</button>
-            <button>Waiting</button>
-            <button>Needs Review</button>
-            <button>Ready To Build</button>
+            <div className="filter-bar">
+                <button>All</button>
+                <button>Not Requested</button>
+                <button>Waiting</button>
+                <button>Needs Review</button>
+                <button>Ready To Build</button>
+            </div>
         </div>
     )
 }
